ca4008cc96c0239e44751cbe60b0b7147a877e5a
Dylan Scott <dylan.scott@gmail.com>
2026-01-06 16:39:58 -0800

    early-init.el

--- early-init.el
+++ early-init.el
@@ -1,5 +1,3 @@
-;; -*- lexical-binding: t -*-
-
 ;;; Startup optimizations
 
 ;; Temporarily increase the garbage collection threshold
@@ -10,3 +8,18 @@
             ;; Restore to defaults (or close to them)
             (setq gc-cons-threshold (* 1024 1024)
                   gc-cons-percentage 0.1)))
+
+;;; UI defaults
+(setq frame-resize-pixelwise t
+      frame-inhibit-implied-resize 'force
+      window-resize-pixelwise t
+      use-file-dialog nil
+      use-short-answers t
+      inhibit-splash-screen t
+      inhibit-startup-screen t
+      inhibit-x-resources t
+      inhibit-startup-echo-area-message user-login-name
+      inhibit-startup-buffer-menu t)
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
